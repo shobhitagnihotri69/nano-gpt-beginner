2bb882bc97a11e873157006a262c94bff720d963
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2025-09-21 16:04:09 +0530

    fix: kv head repeat interleave

--- architecture/gpt2.py
+++ architecture/gpt2.py
@@ -3,7 +3,7 @@ from torch import nn
 
 GPT_CONFIG = {
     'vocab_size': 201088,
-    'context_length': 4000,
+    'context_length': 2048,
     'emb_dim': 1260,
     'n_heads': 12,
     'n_layers': 12,
