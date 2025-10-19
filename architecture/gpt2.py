56c1f9eff2a0255034ccdbc759a5b5a63ba3ee6e
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2025-10-19 13:21:59 +0530

    chore: update gitignore

--- architecture/gpt2.py
+++ architecture/gpt2.py
@@ -4,7 +4,7 @@ from torch import nn
 GPT_CONFIG = {
     'vocab_size': 201088,
     'context_length': 4000,
-    'emb_dim': 1024,
+    'emb_dim': 1260,
     'n_heads': 12,
     'n_layers': 8,
     'drop_rate': 0.1,
