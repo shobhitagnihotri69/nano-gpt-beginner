4a4ef4eafd7e78ee178dae25588955ab8f72c98d
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2026-01-31 16:28:34 +0530

    feat: save best model checkpoint

--- architecture/gptoss.py
+++ architecture/gptoss.py
@@ -7,7 +7,7 @@ from dataclasses import dataclass
 class ModelConfig:
     vocab_size: int = 201088
     hidden_size: int = 1024
-    num_hidden_layers: int = 12
+    num_hidden_layers: int = 8
     num_attention_heads: int = 8
     num_key_value_heads: int = 2
     intermediate_size: int = 1024
