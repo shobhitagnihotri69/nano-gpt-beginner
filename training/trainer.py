73c7606f6bc489f6b7c2503f62177e56c6bf8407
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2026-03-18 09:59:59 +0530

    chore: update readme

--- training/trainer.py
+++ training/trainer.py
@@ -31,7 +31,7 @@ def trainer(model, train_loader, val_loader, device):
     min_lr      = 1e-5
     max_iters   = 5
     warmup      = 100
-    eval_freq   = 200
+    eval_freq   = 150
     eval_iters  = 10
 
     if os.path.exists("model/gptoss.pt"):
