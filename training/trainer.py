386e6a24838256fe24a503c8e53edf4b89b8ec02
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2026-06-11 11:39:09 +0530

    chore: update readme

--- training/trainer.py
+++ training/trainer.py
@@ -27,7 +27,7 @@ def evaluate(model, train_loader, val_loader, device, eval_iter):
     return tl, vl
 
 def trainer(model, train_loader, val_loader, device):
-    lr          = 1e-4
+    lr          = 5e-4
     min_lr      = 1e-5
     max_iters   = 5
     warmup      = 200
