4370225f791edbdce272d1cfa6a9461a401675f9
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2026-07-09 18:43:14 +0530

    fix: expert gate softmax dim

--- training/trainer.py
+++ training/trainer.py
@@ -32,7 +32,7 @@ def trainer(model, train_loader, val_loader, device):
     max_iters   = 5
     warmup      = 200
     eval_freq   = 150
-    eval_iters  = 10
+    eval_iters  = 5
 
     if os.path.exists("model/gptoss.pt"):
         model.load_state_dict(torch.load("model/gptoss.pt"))
