5b0578bf3cbe00800d924d279a82592e299ec521
shobhitagnihotri69 <shobhitagnihotri69@users.noreply.github.com>
2026-07-22 19:05:37 +0530

    feat: save best model checkpoint

--- architecture/gptoss.py
+++ architecture/gptoss.py
@@ -9,7 +9,7 @@ class ModelConfig:
     hidden_size: int = 1024
     num_hidden_layers: int = 12
     num_attention_heads: int = 8
-    num_key_value_heads: int = 4
+    num_key_value_heads: int = 2
     intermediate_size: int = 1024
     num_experts: int = 4
     experts_per_token: int = 2
